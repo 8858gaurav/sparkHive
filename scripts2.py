67315f1a3be7f6898474e8b3f0187f671d0d6698
Gaurav Mishra <gauravmishra010@gmail.com>
2025-08-15 11:32:47 +0530

    when we need to change the warehouse directory while using spark sql or hive terminal

--- scripts2.py
+++ scripts2.py
@@ -6,7 +6,7 @@
 #   </property>
 
 # how to set up a MYSQL in hive
-# this part is pending (under MYSQL, we'll be able to see hive metastore database)
+# this part is pending (under MYSQL, we'll be able to see hive metastore database, and all the metastore info about the db or tables will stored here)
 
 # hive> set hive.execution.engine;
 # hive.execution.engine=mr
@@ -23,3 +23,18 @@
 # FAILED: Execution Error, return code 1 from org.apache.hadoop.hive.ql.exec.DDLTask. MetaException(message:Got exception: org.apache.hadoop.
 # security.AccessControlException Permission denied: user=itv020752, access=WRITE, inode="/user/hive":hdfs:supergroup:drwxr-xr-x
 
+
+# hive> create table misgaurav_orders 
+#     > (order_id string, order_date string, customer_id string, order_status string)
+#     > row format delimited 
+#     > fields terminated by ','
+#     > stored as textfile;
+# FAILED: Execution Error, return code 1 from org.apache.hadoop.hive.ql.exec.DDLTask. MetaException(message:Got exception: org.apache.hadoop.
+# security.AccessControlException Permission denied: user=itv020752, access=WRITE, inode="/user":hdfs:supergroup:drwxr-xr-x
+
+# when we create a table/db by using spark.sql, by default it will create a table in hive, and the data will be stored in hdfs.
+# the location of the data will be /user/<user_name>/warehouse/<db_name>.db/<table_name>/, for this lab it will be /user/itv020752/warehouse/misgaurav_hive.db/misgaurav_orders/
+
+# if we want to create a db/table by using hive cli, then we need to change the default location of the hive metastore directory.
+# hive> set hive.metastore.warehouse.dir=/user/itv020752/warehouse;
+# No rows affected (0.002 seconds)
